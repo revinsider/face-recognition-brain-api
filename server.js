e51522b01e23af774780608af0bd5d1cc6c5d7f0
revinsider <dev@makeover.com.pl>
2022-03-25 10:49:51 +0100

    adding heroku db connection

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-cubic-20050",
-    port: 5432,
-    user: "postgres",
-    password: "test",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
